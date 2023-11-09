4570a4f42f8c03a3fa15a231f7359567803ac6d7
Alvaro <alvarord519@gmail.com>
2023-11-08 23:44:32 -0500

    Agregando navbar y primeras vistas

---
+++ src/app/about/page.tsx
@@ -0,0 +1,16 @@
+const About = () => {
+    return ( 
+        <div className="h-[2000px] bg-gray-950">
+            <div className="min-h-[1000px]">
+                <div className="flex lg:sticky top-20 p-8 bg-white ">
+                    Hola
+                </div>
+            </div>
+            <div className="min-h-[1000px]">
+
+            </div>
+        </div>
+     );
+}
+ 
+export default About;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,94 @@
-import Image from 'next/image'
+import { raleway } from "@/ui/fonts";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="flex flex-col ">
+      <section className="min-h-[600px] bg-gray-200 relative overflow-x-hidden">
+        <div className="w-full h-32"></div>
+
+        <div className="relative z-20 p-8 lg:max-w-[1280px] mx-auto">
+          <div className="flex move-in">
+            <div className="h-12 w-1 mt-4 mr-14 bg-red-500" />
+            <h1 className={`text-[52px] font-bold ${raleway.className}`}>
+              Máximo Cuidado para <br /> tu Vehículo
+            </h1>
+          </div>
         </div>
-      </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+        <div className="lg:max-w-[1280px] mx-auto relative flex z-20 px-8 mb-24 move-in">
+          <div className="lg:ml-16 bg-red-500 text-white w-[200px] flex items-center justify-center py-5 font-medium cursor-pointer">
+            Contactanos
+          </div>
+        </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+        <div className="lg:max-w-[1200px] mx-auto text-[13px] font-medium flex pl-14">
+          <div className="mr-20">
+            <h3>contacto@detailcenter.pe</h3>
+            <span className="text-red-400">987-654-321</span>
+          </div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+          <div>
+            <h3>Direccion</h3>
+            <span className="text-red-400">Av Falsa 123 - Lima</span>
+          </div>
+        </div>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
+        <div className="absolute top-20 right-0 z-20"></div>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+        <p className="text-[113px] xl:text-[280px] text-gray-50 absolute xl:top-20 -left-12 font-medium opacity-80 slow-motion fading-slow-mo">
+          DetailCenter
+        </p>
+      </section>
+
+      <section className="bg-[#1a1a1a] min-h-[600px] w-full relative antialiased ">
+        <div className="min-w-[1200px] w-full h-[605px] object-cover">
+          <img
+            src="./car-washing.avif"
+            alt="washing"
+            className=" object-fill h-[605px]"
+          />
+        </div>
+      </section>
+
+      <section className="bg-[#1a1a1a] text-white w-full antialiased pt-20">
+        <div className="lg:max-w-[1280px] grid lg:grid-cols-12 justify-between mx-auto px-8">
+          <div className="sticky top-20 flex flex-col text-white col-span-4">
+            <div className="flex">
+              <div className="h-12 w-1 mt-4 mr-14 bg-red-500" />
+              <div>
+                <h2
+                  className={`text-[52px] font-semibold ${raleway.className}`}
+                >
+                  Nuestros <br /> Servicios
+                </h2>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Maiores porro dicta animi quos at provident atque? Omnis,
+                  magni cupiditate? Laudantium nam cupiditate voluptates vitae
+                  veniam inventore soluta quam porro aliquid!
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-span-7 min-h-[2000px]">
+            <div className="flex gap-10">
+              <div className="min-w-[260px] max-w-[260px]">
+                <img src="./paint-fixing.webp" alt="paint-fixing" />
+              </div>
+
+              <div className="flex flex-col gap-4">
+                <h3 className="text-xl font-semibold">Correcion de Pintura</h3>
+                <p className="text-sm">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque
+                  laboriosam sunt optio nesciunt nam veritatis iste quibusdam
+                  quos non voluptatem tenetur.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </main>
-  )
+  );
 }
